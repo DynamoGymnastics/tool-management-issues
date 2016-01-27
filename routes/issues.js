02791a0d479f6f588feba0e561f6713e6c6ba1c6
Michael Perrotte <mike@mikecorp.ca>
2016-01-27 15:07:44 -0500

    added necessary helper functions

--- routes/issues.js
+++ routes/issues.js
@@ -1,30 +1,63 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const Config = require('../libs/Config');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('issues', { menu: 'issues' });
+
+  // TODO: check session for error
+  Config.getIssues((err, issues) => {
+    let model = {
+      menu: null,
+      issues: null,
+      require: require
+    };
+    model.menu = 'issues';
+    if (err) {
+      model.issues = [{}];
+      console.log('err:', err);
+    } else {
+      model.issues = issues;
+    }
+    res.render('issues', model);
+  });
+  // res.render('issues', { menu: 'issues' });
 });
 
 router.get('/create', function(req, res, next) {
-
-  // TODO: make call to API for issues
-  // TODO: get array of labels
-  // TODO: add labels to view model
-  res.render('issues/create', { menu: 'issues' });
+  let model = {
+    menu: null,
+    labels: null
+  };
+  model.menu = 'issues';
+  Config.getLabels((err, labels) =>{
+    if (err) {
+      model.labels = [];
+    } else {
+      model.labels = labels;
+    }
+    res.render('issues/create', model);
+  });
 });
 
 router.post('/create', function(req, res, next) {
   let issue = req.body;
+  console.log(issue);
 
   // TODO: sanitize all data
-  // TODO: send request to API
-  // TODO: wait for reply
-  // TODO: display something nice for the user
-  console.log(issue);
-  res.redirect('/issues');
-  // res.redirect('back');
+  Config.createIssue(issue, (err, data) => {
+    if (err) {
+
+      // TODO: tell the user we got an error
+      console.log('tell you user we got an error.');
+      req.params.error = 'CreateError';
+    } else {
+
+      // TODO: display something nice for the user
+      res.redirect('/issues');
+    }
+  });
 });
 
 module.exports = router;
