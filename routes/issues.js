4ab43109588f6260460a9f5833bd9a34fc69b64c
Michael Perrotte <mike@mikecorp.ca>
2016-01-27 06:02:06 -0500

    created workflow

--- routes/issues.js
+++ routes/issues.js
@@ -8,13 +8,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/create', function(req, res, next) {
+
+  // TODO: make call to API for issues
+  // TODO: get array of labels
+  // TODO: add labels to view model
   res.render('issues/create', { menu: 'issues' });
 });
 
 router.post('/create', function(req, res, next) {
   let issue = req.body;
+
+  // TODO: sanitize all data
+  // TODO: send request to API
+  // TODO: wait for reply
+  // TODO: display something nice for the user
   console.log(issue);
-  res.render('issues', { menu: 'issues' });
+  res.redirect('/issues');
+  // res.redirect('back');
 });
 
 module.exports = router;
